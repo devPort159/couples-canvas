f383783e3a742b8176c4a41b26f23f7139b5dd74
Andrew Porter <aporter159@gmail.com>
2025-10-17 12:53:26 -0700

    import ID as type only

--- convex/canvases.ts
+++ convex/canvases.ts
@@ -1,6 +1,6 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
-import { Id } from "./_generated/dataModel";
+import type { Id } from "./_generated/dataModel";
 
 function randomSlug(len = 6) {
 	// URL-safe base36-ish short id
